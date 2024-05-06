efd767148ea75bc8744264b88f9c3440c00bb86c
unknown <chua.avrilnigel0412@gmail.com>
2024-05-06 16:00:37 +0800

    Scrape All ENglish Meanings

--- src/main/java/com/example/kanjiscraper/ScrapperClass.java
+++ src/main/java/com/example/kanjiscraper/ScrapperClass.java
@@ -6,35 +6,46 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ScrapperClass {
     String url;
+
     Document document;
     String cssQueryForKanji = ".character-grid";
     ScrapperClass(){
         url = "https://www.wanikani.com/kanji?difficulty=pleasant";
+
         try {
             document = Jsoup.connect(url).get();
             Elements kanjiData = document.select(cssQueryForKanji);
-            for(Element k : kanjiData){
+            for (Element k : kanjiData) {
                 String level = k.select(".character-grid__header-text").text();
                 System.out.println(level);
                 Elements dataElements = k.select(".subject-character__content");
-                for(Element data : dataElements){
+                for (Element data : dataElements) {
                     String kanji = data.select(".subject-character__characters").text();
                     String furigana = data.select(".subject-character__reading").text();
-                    String english = data.select(".subject-character__meaning").text();
+                    String englishMeaningURL = "https://www.wanikani.com/kanji/" + kanji;
+                    Document englishDocument = Jsoup.connect(englishMeaningURL).get();
+                    String getCssQueryForEnglishMeaning = ".subject-section__meanings-items";
+                    Elements englishMeaningData = englishDocument.select(getCssQueryForEnglishMeaning);
+                    StringBuilder englishMeanings = new StringBuilder();
+                    int size = englishMeaningData.size();
+                    for(int i = 0; i < size; i++){
+                        String english = englishMeaningData.get(i).text();
+                        englishMeanings.append(english);
+                        if(i < size - 1){
+                            englishMeanings.append(", ");
+                        }
+                    }
                     System.out.println("Kanji: " + kanji);
                     System.out.println("Furigana: " + furigana);
-                    System.out.println("English: " + english);
-                    if(DatabaseUtilities.insertJapaneseLearningData(level, kanji, furigana, english)){
-                        System.out.println("Success!");
-                    }
-                    System.out.println("\n");
+                    System.out.println("English: " + englishMeanings);
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
     public static void main(String[] args) {
